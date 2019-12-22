65943ebc2f852a415387df33ea5c2a087e379262
Xander Deng <dengxiang2010@gmail.com>
2019-12-22 16:24:47 +0800

    refine MatchResult.Capture

--- Sources/Regex/MatchResult.swift
+++ Sources/Regex/MatchResult.swift
@@ -3,8 +3,42 @@ import Foundation
 public struct MatchResult: Equatable, Hashable {
     
     public struct Capture: Equatable, Hashable {
+        
+        fileprivate enum Content: Equatable, Hashable {
+            
+            case valid(substring: Substring)
+            
+            case invalid(original: NSString)
+            
+            static func validate(string: String, range: NSRange) -> Content {
+                if let r = Range(range, in: string) {
+                    return .valid(substring: string[r])
+                } else {
+                    return .invalid(original: string as NSString)
+                }
+            }
+        }
+        
+        fileprivate let content: Content
+        
         public let range: NSRange
-        public let content: Substring?
+        
+        public var string: String {
+            switch content {
+            case let .valid(substring):
+                return String(substring)
+            case let .invalid(original):
+                return original.substring(with: range)
+            }
+        }
+        
+//        public var content: Substring? {
+//            if case let .valid(substring) = _content else {
+//                return substring
+//            } else {
+//                return nil
+//            }
+//        }
     }
     
     public let captures: [Capture?]
@@ -14,33 +48,19 @@ public struct MatchResult: Equatable, Hashable {
         captures = (0..<result.numberOfRanges).map { index in
             let nsRange = result.range(at: index)
             guard nsRange.location != NSNotFound else { return nil }
-            return Capture(range: nsRange, content: string[nsRange])
+            return Capture(content: .validate(string: string, range: nsRange), range: nsRange)
         }
     }
-}
-
-extension MatchResult {
     
     public var range: NSRange {
         return captures[0].unsafelyUnwrapped.range
     }
     
-    public var content: Substring? {
-        return captures[0].unsafelyUnwrapped.content
-    }
-    
     public var string: String {
-        return content.map(String.init) ?? ""
+        return captures[0].unsafelyUnwrapped.string
     }
     
     public subscript(_ captureGroupIndex: Int) -> Capture? {
         return captures[captureGroupIndex]
-    }
-}
-
-extension MatchResult.Capture {
-    
-    public var string: String {
-        return content.map(String.init) ?? ""
     }
 }
--- Sources/Regex/Utilities.swift
+++ Sources/Regex/Utilities.swift
@@ -5,11 +5,4 @@ extension String {
     var fullRange: NSRange {
         return NSRange(location: 0, length: utf16.count)
     }
-    
-    subscript(nsRange: NSRange) -> Substring? {
-        guard let range = Range(nsRange, in: self) else {
-            return nil
-        }
-        return self[range]
-    }
 }
