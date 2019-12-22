4bd66846ccdc71200de61ae1845e14f293a37dee
Xander Deng <dengxiang2010@gmail.com>
2019-12-22 17:44:26 +0800

    add: test case

--- Tests/RegexTests/RegexTests.swift
+++ Tests/RegexTests/RegexTests.swift
@@ -1,19 +1,52 @@
 import XCTest
-@testable import Regex
+import Regex
 
 class RegexTests: XCTestCase {
     
-    func testPatternMatching() {
-        switch "foo" {
-        case try! Regex("f.o"):
-            break
-        default:
-            XCTFail()
+    func testMatches() {
+        let source = "foo"
+        let regex = try! Regex("f.o")
+        
+        XCTAssertTrue(regex.isMatch(source))
+        XCTAssertTrue(regex ~= source)
+        
+        switch source {
+        case regex: break
+        default:    XCTFail()
         }
     }
     
+    func testReplace() {
+        let source = "123 foo fo0 bar"
+        let regex = try! Regex("(foo|bar)")
+        let result = source.replacingMatches(of: regex, with: "$1baz")
+        XCTAssertEqual(result, "123 foobaz fo0 barbaz")
+    }
+    
+    func testCaptureGroup() {
+        let source = "123 foo bar baz"
+        let regex = try! Regex(#"(\d+)(boo)? (foo) bar"#)
+        let match = regex.firstMatch(in: source)!
+        XCTAssertEqual(match.string, "123 foo bar")
+        XCTAssertEqual(match.captures.count, 4)
+        XCTAssertEqual(match[0]?.string, "123 foo bar")
+        XCTAssertEqual(match[1]?.string, "123")
+        XCTAssertNil(match[2])
+        XCTAssertEqual(match[3]?.string, "foo")
+    }
+    
+    func testExtendedGraphemeClusters() {
+        let source = "cafe\u{301}" // café
+        let regex = try! Regex("caf.")
+        let match = regex.firstMatch(in: source)!
+        XCTAssertEqual(match.string, "cafe")
+        XCTAssertNil(Range(match.range, in: source))
+    }
     
     static var allTests = [
-        ("testPatternMatching", testPatternMatching),
+        ("testMatches", testMatches),
+        ("testReplace", testReplace),
+        ("testCaptureGroup", testCaptureGroup),
+        ("testExtendedGraphemeClusters", testExtendedGraphemeClusters),
     ]
 }
