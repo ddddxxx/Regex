da909488baf4e93306e9d7666ceec077eeb91eb5
Xander Deng <dengxiang2010@gmail.com>
2019-12-22 17:03:14 +0800

    add: documentation

--- Sources/Regex/MatchResult.swift
+++ Sources/Regex/MatchResult.swift
@@ -1,13 +1,19 @@
 import Foundation
 
+/// `MatchResult` represents a single match of a regex.
 public struct MatchResult: Equatable, Hashable {
     
+    /// `Capture` represents a captured string for a single match.
     public struct Capture: Equatable, Hashable {
         
         fileprivate enum Content: Equatable, Hashable {
             
+            /// Valid captured substring.
             case valid(substring: Substring)
             
+            /// Captured string breaks extended grapheme cluster thus the substring cannot be formed.
+            ///
+            /// The original string is stored for later slicing.
             case invalid(original: NSString)
             
             static func validate(string: String, range: NSRange) -> Content {
@@ -19,10 +25,13 @@ public struct MatchResult: Equatable, Hashable {
             }
         }
         
+        /// Captured content.
         fileprivate let content: Content
         
+        /// The matching range.
         public let range: NSRange
         
+        /// The matched string.
         public var string: String {
             switch content {
             case let .valid(substring):
@@ -32,15 +41,21 @@ public struct MatchResult: Equatable, Hashable {
             }
         }
         
-//        public var content: Substring? {
-//            if case let .valid(substring) = _content else {
-//                return substring
-//            } else {
-//                return nil
-//            }
-//        }
+        /*
+        public var content: Substring? {
+            if case let .valid(substring) = _content else {
+                return substring
+            } else {
+                return nil
+            }
+        }
+         */
     }
     
+    /// A group of captured strings for a single match.
+    ///
+    /// The 0th capture always corresponds to the entire match. A capture group might be `nil` If it didn't
+    /// participate in the match.
     public let captures: [Capture?]
     
     init(result: NSTextCheckingResult, in string: String) {
@@ -52,10 +67,12 @@ public struct MatchResult: Equatable, Hashable {
         }
     }
     
+    /// The range of the entire match.
     public var range: NSRange {
         return captures[0].unsafelyUnwrapped.range
     }
     
+    /// The entire matched string.
     public var string: String {
         return captures[0].unsafelyUnwrapped.string
     }
