f3d1e66a7e186056de504e619c1a5a83dba8ebcb
Xander Deng <dengxiang2010@gmail.com>
2021-02-24 18:34:25 +0800

    add documentation

--- Sources/Regex/Regex.swift
+++ Sources/Regex/Regex.swift
@@ -7,10 +7,20 @@
 
 import Foundation
 
+/// A simple wrapper around `NSRegularExpression`.
 public struct Regex: Equatable, Hashable {
     
+    /// These constants define the regular expression options.
     public typealias Options = NSRegularExpression.Options
+    
+    /// The matching options constants specify the reporting, completion and
+    /// matching rules to the expression matching methods. These constants are
+    /// used by all methods that search for, or replace values, using a regular
+    /// expression.
     public typealias MatchingOptions = NSRegularExpression.MatchingOptions
+    
+    /// Set by the Block as the matching progresses, completes, or fails. Used
+    /// by the method `enumerateMatches(in:options:range:using:)`.
     public typealias MatchingFlags = NSRegularExpression.MatchingFlags
     
     let regularExpression: NSRegularExpression
@@ -19,11 +29,24 @@ public struct Regex: Equatable, Hashable {
         regularExpression = regex
     }
     
+    /// Create a regular expression with the specified pattern and options.
+    ///
+    /// - Parameters:
+    ///   - pattern: The regular expression pattern to compile.
+    ///   - options: The regular expression options that are applied to the
+    ///   expression during matching.
+    /// - Throws: Throws an NSError object if the regular expression pattern is
+    /// invalid
     @_disfavoredOverload
     public init(_ pattern: String, options: Options = []) throws {
         self.init(try NSRegularExpression(pattern: pattern, options: options))
     }
     
+    /// Create a regular expression with the static pattern and options.
+    ///
+    /// This method doesn't throw since you use a string literal as the input.
+    /// An invalid regular expression pattern is considered to be an program
+    /// error and result in a crash.
     public init(_ staticPattern: StaticString, options: Options = []) {
         do {
             try self.init(staticPattern.description, options: options)
@@ -31,22 +54,46 @@ public struct Regex: Equatable, Hashable {
             preconditionFailure("failed to create regex: \(error)")
         }
     }
-}
-
-extension Regex {
     
+    /// The regular expression pattern.
     public var pattern: String {
         return regularExpression.pattern
     }
     
+    /// The options used when the regular expression option was created.
+    ///
+    /// The options property specifies aspects of the regular expression
+    /// matching that are always used when matching the regular expression. For
+    /// example, if the expression is case sensitive, allows comments, ignores
+    /// metacharacters, etc.
     public var options: Options {
         return regularExpression.options
     }
     
+    /// The number of capture groups in the regular expression.
+    ///
+    /// A capture group consists of each possible match within a regular
+    /// expression. Each capture group can then be used in a replacement
+    /// template to insert that value into a replacement string.
+    ///
+    /// This value puts a limit on the values of n for `$n` in templates, and it
+    /// determines the number of captures in the returned `Match` instances
+    /// returned in the `match...` methods.
     public var numberOfCaptureGroups: Int {
         return regularExpression.numberOfCaptureGroups
     }
     
+    /// Returns a string by adding backslash escapes as necessary to protect any
+    /// characters that would match as pattern metacharacters.
+    ///
+    /// Returns a string by adding backslash escapes as necessary to the given
+    /// string, to escape any characters that would otherwise be treated as
+    /// pattern metacharacters. You typically use this method to match on a
+    /// particular string within a larger pattern.
+    ///
+    /// For example, the string `"(N/A)"` contains the pattern metacharacters
+    /// `(`, `/`, and `)`. The result of adding backslash escapes to this string
+    /// is `#"\(N\/A\)"#`.
     public static func escapedPattern(for string: String) -> String {
         return NSRegularExpression.escapedPattern(for: string)
     }
